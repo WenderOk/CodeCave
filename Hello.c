5a6ec59f53a86058f18987039916827d662888b4
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-06-08 00:10:50 +0300

    aboba

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+int main(void)
+{
+     int A;;
+     scanf("%i", &A);
+     printf("Hello, %i\n", A);
+     getchar();
+}
---
+++ ILOVECHARS.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char String[]="VS, dont you speak russian?";
+    for (int i = 0; i < 28; i++)
+    {
+        printf("%c",String[i]);
+    }
+       printf("\n");
+}
