958c8b29f7c003657e1363f8a8385539b606b9c4
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-06-30 19:51:22 +0300

    RunningSum

---
+++ C/RunningSum/RunningSum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* runningSum(int* nums, int numsSize, int* returnSize){
+nums = (int*) malloc(numsSize * sizeof(int));
+returnSize = (int*) malloc(numsSize * sizeof(int));
+
+for(int i; i < numsSize; i++)
+    {
+        //returnSize[i] += nums[i];
+    }
+return(nums[1]);
+}
