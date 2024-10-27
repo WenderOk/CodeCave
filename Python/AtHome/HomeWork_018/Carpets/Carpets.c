387cfbb6093c51b1be4ec5fa11d44e222d6b4d6a
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-10-27 14:29:59 +0300

    HomeWork_025
    It was good

--- Python/AtHome/HomeWork_018/Carpets/Carpets.c
+++ Python/AtHome/HomeWork_018/Carpets/Carpets.c
@@ -1,13 +1,20 @@
 #include <conio.h>
 #include <stdio.h>
-int a, b, c;
+int a, b, c, ak, bk, result;
 
 int main(void){
     printf("Enter length and width of the room and the side of carpet: \n");
-    scanf("%i", &a);
-    scanf("%i", &b);
-    scanf("%i", &c);
-    printf("%i\nEnter any key to continue", (a/c) * (b/c));
+    scanf("%d %d %d", &a,&b,&c);
+    while (a > 0){
+        ak++;
+        a-=c;
+    }
+    while (b > 0){
+        bk++;
+        b-=c;
+    }
+    for(int i = 0; i < bk; i++){ result += ak; }
+    printf("%i\nEnter any key to continue\n", result);
     getch();
 }
 //Damn, I write in English...
--- Python/AtHome/HomeWork_018/SoLongZeros2.py
+++ Python/AtHome/HomeWork_018/SoLongZeros2.py
@@ -1,14 +1,10 @@
 # Вариант программы, который переводит введенное в десятичной системе счисления число в число 
 # в пятиричной системе счисления
-n,mx,c,s = int(input()),-1,0,''
-while n > 0:
-    s+=str(n%5)
-    n//= 5
-n=int(s)
-print(n)
+n,mx,c = int(input()),-1,0
 for _ in range(len(str(n))):
-    if n % 10 == 0: c+=1
+    if n % 5 == 0: c+=1
     else: c = 0
     n //= 10
     if c>mx: mx=c
+    print(n)
 print(mx)
