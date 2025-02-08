aba90f1427935a68d5ce9b09552d2acf4b8adc94
WenderOk <wondergeorgy@yandex.ru>
2025-02-08 22:32:18 +0300

    New_Web

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,10 +1,6 @@
-
-
-
-a =13246884
-A=[]
-for i in str(a):
-    A.append(int(i))
-mx = max(A)
-n = A.count(mx)
-print(n)
+A, B = [x for x in input("Введите исходную строку: ")], []
+#if A[-1] == A[0]: B.append(A[0])
+for i in range(len(A)):
+    if A[i] != A[i+1]: B.append(A[i])
+print(*A, sep="")
+print(*B, sep="")
