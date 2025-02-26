f5bd57ed0cc5d87844652558c94f264ca9aac76f
WenderOk <wondergeorgy@yandex.ru>
2025-02-26 11:40:20 +0300

    JSUPDATE

--- Web/JS-Page/script.js
+++ Web/JS-Page/script.js
@@ -1,9 +1,22 @@
+// "use srtict";
+// {
+//   function GetNumber() {
+//     let num = prompt("Введите число");
+//     return num;
+//   }
+// }
+
+// function writeNumber(num) {
+//   for (let i = 0; i <= num; i++) console.log(i);
+// }
+// writeNumber(GetNumber());
+/*
 let user = {};
 user = { name:"John", 
     surname:"Smith",};
 user.name = "Pete";
 delete user.name;
-/*
+
 let age = +prompt("Введите ваш возраст:");
 if (18 <= age) {
   alert("Все в порядке");
@@ -46,3 +59,35 @@ for (let i = 0; i <= a; i++) {
 //   age: 16,
 //   group: "SD",
 // };
+
+// let arr = [];
+// let arr2 = [5, -8, 7, 1];
+
+// for (let i = 0; i < arr2.length; i++) console.log(arr2[i]);
+
+// let set = new Set();
+
+// set.add("name");
+// set.add("Georgy");
+// set.add("asdfasdfasdfasdf");
+// set.add("asdfasdfasdfasdf");
+
+// for (let item of set) {
+//   console.log(item);
+// }
+
+// function unique(arr) {
+//   let strings = new Set();
+//   for (let i = 0; i < arr.length; i++) {
+//     strings.add(arr[i]);
+//   }
+//   for (let item of strings) {
+//     console.log(item);
+//   }
+// }
+
+// unique([12, 32, 12, 12, 23, 44, 36, 567, 245, 867]);
+
+console.log("asd".length);
+console.log("asd".toLowerCase());
+console.log("asd".toUpperCase());
