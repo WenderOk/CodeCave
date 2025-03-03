4ce13885279611437d304a2f6d6f2e5b3e52aa7b
WenderOk <wondergeorgy@yandex.ru>
2025-03-03 23:42:21 +0300

    Some_JS

--- Web/JS-Page/script.js
+++ Web/JS-Page/script.js
@@ -1,48 +1,13 @@
-let user = {};
-user = { name:"John", 
-    surname:"Smith",};
-user.name = "Pete";
-delete user.name;
-/*
-let age = +prompt("Введите ваш возраст:");
-if (18 <= age) {
-  alert("Все в порядке");
-} else if (age < 5) {
-  alert("Верни маме карту!");
-} else {
-  alert("Вы несовершенолетний");
+let student = {
+  name: "Владислав",
+  age: 16,
+  group: "РПО",
+};
+
+function writeAllFields(obj) {
+  for (let key in obj) {
+    console.log(key + ": " + obj[key]);
+  }
 }
 
-
-let i = 1;
-
-while (i < 102) {
-  console.log(i);
-  i++;
-}
-
-
-a = prompt("Сколько чисел вывесати: ");
-for (let i = 0; i <= a; i++) {
-  console.log(i);
-}
-*/
-
-// function GetNumber() {
-//   let n = +prompt("Ввдеите число: ");
-//   return n;
-// }
-// function PrintNums(n) {
-//   for (let i = 0; i <= n; i++) {
-//     console.log(i);
-//   }
-//   return "Все числа выведены!";
-// }
-
-// console.log(PrintNums(GetNumber()));
-
-// let user = {
-//   name: "Georgy",
-//   age: 16,
-//   group: "SD",
-// };
+writeAllFields(student);
