1538fce14a5b03280c56dd847746065a37a7d878
WenderOk <wondergeorgy@yandex.ru>
2025-04-30 11:39:12 +0300

    More Machine Learning

--- C/HelloWorld/Hello.c
+++ C/HelloWorld/Hello.c
@@ -1,12 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-     char Name[80];
-
-     printf("Write your name please: ");
-     scanf("%s", Name);
-     printf("Hello, %s\n", Name);
+     printf("Hello, World!");
 }
--- Python/Hello.py
+++
@@ -1,7 +0,0 @@
-from random import*
-N = int(input())
-c, A = 0, [randint(-100, 100) for _ in range(N)]
-for _ in range(A.count(0)): A.remove(0)
-for i in range(len(A)):
-    if ((A[i] > 0 and A[i-1] < 0) or (A[i] < 0 and A[i-1] > 0)): c+=1
-print(A, '\n', c, sep='')
