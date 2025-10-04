f1cbee26c6a56107e5a6a0238f158ced260e351f
WenderOk <wondergeorgy@yandex.ru>
2025-10-04 19:44:32 +0300

    CW-CPP-04-10-25

---
+++ C++/Syntax/SomeSyntax.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    // initialization with 0
+    // int x{};
+
+    // // constant
+    // const double PI{3.14};
+
+    // // ">>" is input "<<" is output
+
+    // std::cout << "value of variable: " << x << "\n";
+
+    // std::cout << "Enter line and column: ";
+    // int col{};
+    // int line{};
+    // std::cin >> line >> col;
+    // std::cout << "You enter: " << line << " " << col << "\n";
+
+
+    // // integer dividing
+    // std::cout << 5 / 2 << "\n"; // 2
+    
+    // std::cout << 5 % 2 << "\n"; // 1
+    // // float dividing
+    // std::cout << 5.0 / 2 << "\n"; // 2.5
+    
+    int x{};
+    x=100.99;
+    std::cout << x << "\n";
+
+    int y = 5'000'000'000;
+    std::cout << y << "\n";
+
+    return 0;
+}
