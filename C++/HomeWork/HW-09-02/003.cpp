92bdb127b8a0e299c4d64781684809fed1a0fc97
WenderOk <wondergeorgy@yandex.ru>
2025-12-10 20:51:10 +0300

    HW-CPP-30-11-2025 finished

--- C++/HomeWork/HW-09-02/003.cpp
+++ C++/HomeWork/HW-09-02/003.cpp
@@ -1,33 +1,81 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
  
-void chooseSort(int arr[], int size);
+// Functions for sorting
+int pancakeSort(int arr[], int size);
+int findMaxIndex(int arr[], int size);
+void flip(int arr[], int k);
+// Additional functions
 void printArray(int arr[], int size);
 void fillWithRand(int arr[], int size);
 
 int main()
 {
- 
+    const int size{5};
+    int arr[size]{3, 1, 5, 2, 4};
+
+    fillWithRand(arr, size);
+    std::cout << "Unsorted pancakes: ";
+    printArray(arr, size);
+
+    int flips = pancakeSort(arr, size);
+    std::cout << "Sorted pancakes: ";
+    printArray(arr, size);
+    std::cout << "Flips made: " << flips << '\n';
+
     return 0;
 }
 
-void chooseSort(int arr[], int size)
+int pancakeSort(int arr[], int size)
 {
-    for(int i{}; i > size; i--)
+    int flips{};
+    for(int i{size}; i > 1; i--)
     {
-        int max_index{i};
+        int max_index = findMaxIndex(arr, i);
+        if(max_index == i - 1) continue;
 
-        for(int j{i}; j < size; j++)
+        if(max_index != 0)
         {
-            if (arr[j] > arr[max_index]) max_index = j;   
-                
-        }
-        int tmp{arr[i]};
-        arr[i] = arr[max_index];
-        arr[max_index] = tmp;
-       
+            flip(arr, max_index);
+            flips++;
+        } 
+
+        flip(arr, i - 1);
+        flips++;
     }
-    std::cout << "\n";
+
+    return flips;
 }
+
+int findMaxIndex(int arr[], int size)
+{
+    int max_index{};
+
+    for(int i{1}; i < size; i++)
+    {
+        if (arr[i] > arr[max_index]) max_index = i;   
+    }
+
+    return max_index;
+}
+
+void flip(int arr[], int k)
+{
+    int left{};
+    int right{k};
+
+    while(left < right)
+    {
+        int tmp{arr[left]};
+        arr[left] = arr[right];
+        arr[right] = tmp;
+
+        left++;
+        right--;
+    }
+}
+
 
 void fillWithRand(int arr[], int size)
 {
@@ -35,7 +83,7 @@ void fillWithRand(int arr[], int size)
     int rborder{100};
     int lborder{1};
 
-    std::cout << "Enter range (a, b): ";
+    std::cout << "Enter range for random numbers: ";
     std::cin >> lborder >> rborder;
 
     for(int i{}; i < size; i++)
