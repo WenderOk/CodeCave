8d77a2cda43f4285c793c4caeef634bea8fdd111
WenderOk <wondergeorgy@yandex.ru>
2026-03-26 13:53:59 +0300

    CW-CS-23-03

---
+++ C#/Lessons/23-03/College.cs
@@ -0,0 +1,19 @@
+namespace College
+{
+    namespace IT_Top
+    { 
+        class College
+        { 
+            enum subjects { math, literature, programming}
+        }
+    }
+
+    namespace Pushkino
+    {
+        class College
+        {
+            enum subjects { math, literature, programming }
+
+        }
+    }
+}
