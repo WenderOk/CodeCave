3d5a7d239c59fb8281a8cff0ccfd4d4b952c6612
WenderOk <wondergeorgy@yandex.ru>
2026-05-28 13:19:44 +0300

    CW-CS-28-05

---
+++ C#/Lessons/28-05/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace _28_05
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textLabel.Text = string.Empty;
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                    ((Button)el).Click += Button_Click;
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+                textLabel.Text = "";
+            else if(str == "=")
+            {
+                string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value;
+            }
+            else
+               textLabel.Text += str;
+        }
+    }
+}
---
+++ C#/Lessons/28-05_1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _28_05_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string Digits = "0123456789";
+        private const string SpecChars = "!@#$%^&*()_+=-[]{}|;:',.<>?";
+
+        private readonly Random _random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int length = (int)slider_len.Value;
+            StringBuilder charPool = new StringBuilder();
+
+            if (checkBox_letters.IsChecked == true) charPool.Append(Letters);
+            if (checkBox_digits.IsChecked == true) charPool.Append(Digits);
+            if (checkBox_spec.IsChecked == true) charPool.Append(SpecChars);
+
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                int index = _random.Next(charPool.Length);
+                password.Append(charPool[index]);
+            }
+
+            textBox_pwd.Text = password.ToString();
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            textBox_pwd.Clear();
+        }
+    }
+}
