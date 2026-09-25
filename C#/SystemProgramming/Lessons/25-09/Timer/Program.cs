21bb3b4e553c88c7542ac229176012c5e5159de0
WenderOk <wondergeorgy@yandex.ru>
2026-09-25 15:30:52 +0300

    CW-CS-25-09

---
+++ C#/SystemProgramming/Lessons/25-09/Timer/Program.cs
@@ -0,0 +1,12 @@
+﻿TimerCallback timerCallback = new TimerCallback(TimerTick);
+
+Timer timer = new Timer(timerCallback);
+
+timer.Change(2000, 500);
+
+Console.ReadKey();
+
+void TimerTick(object? state)
+{
+    Console.WriteLine("сообщение по таймеру");
+}
